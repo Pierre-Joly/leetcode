02147cfa8730109a0c6f94071367dd7b266670e5
pjoly <pierre.joly@ecl21.ec-lyon.fr>
2024-04-17 14:23:55 +0200

    Add Two Numbers using ternary in Java

--- algorithms/java/AddTwoNumbers.java
+++ algorithms/java/AddTwoNumbers.java
@@ -50,9 +50,9 @@ class Solution {
         int carry = 0;
 
         while (l1 != null || l2 != null || carry != 0) {
-            int val1 = (l1 != null) ? l1.val : 0;
-            int val2 = (l2 != null) ? l2.val : 0;
-            int sum = val1 + val2 + carry;
+            int sum = carry;
+            sum += (l1 != null) ? l1.val : 0;
+            sum += (l2 != null) ? l2.val : 0;
 
             carry = sum / 10;
             current.next = new ListNode(sum % 10);
